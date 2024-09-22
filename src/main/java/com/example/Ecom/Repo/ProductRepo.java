764712bd2d75ffe9979a2931ddbc515c9f16ee5f
Jayantyadav26 <95078136+Jayantyadav26@users.noreply.github.com>
2024-09-22 22:25:10 +0530

    Initial commit

---
+++ src/main/java/com/example/Ecom/Repo/ProductRepo.java
@@ -0,0 +1,23 @@
+package com.example.Ecom.Repo;
+
+
+import com.example.Ecom.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+    //searching product without the primary key.
+    //JPQL
+    @Query("SELECT p FROM Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))"
+    )
+
+    List<Product> searchProducts(String keyword);
+}
---
+++ src/main/resources/data1.sql
@@ -0,0 +1,2 @@
+INSERT INTO product(name, description, brand, price, category, releaseDate, productAvailable, stockQuantity)
+VALUES ('XUV300', 'A features loaded car', 'Mahindra', 1300000, 'Compact SUV', '2020-12-25', true, 20);
